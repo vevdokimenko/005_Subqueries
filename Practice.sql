e30ad13b9f1124add927ada1bb411c46b206a27d
vevdokimenko <evdokimenko@hotmail.com>
2021-12-07 20:28:43 +0200

    Practice

--- Practice.sql
+++ Practice.sql
@@ -18,15 +18,41 @@ FROM tmpAuthors;
 
 # 3. Показать среднеарифметическую цену продажи всех книг. Результат сохранить в временную таблицу.
 CREATE TEMPORARY TABLE avgPriceBook
-SELECT avg(Price) FROM Sales;
+SELECT AVG(Price)
+FROM Sales;
 
-SELECT * FROM avgPriceBook;
+SELECT *
+FROM avgPriceBook;
 
 # 4. Вывести информацию о книгах по «Computer Science» с наибольшим количеством страниц.
+SELECT NameTheme, NameBook, MAX(Pages)
+FROM Books
+         JOIN Themes T ON T.ID_THEME = Books.ID_THEME
+GROUP BY NameTheme;
+
 # 5. Показать на экран сумму страниц по каждой из тематик, при этом учитывать только книги одной из следующих тематик: «Computer
-# Science», «Science Fiction», «Web Technologies» и с количеством страниц более 300.
+# Science», «Science Fiction», «Web Development» и с количеством страниц более 300.
+SELECT NameTheme, NameBook, SUM(Pages)
+FROM Books
+         JOIN Themes T ON T.ID_THEME = Books.ID_THEME
+WHERE NameTheme IN ('Computer Science', 'Science Fiction', 'Web Development')
+GROUP BY NameTheme;
+
 # 6. Создать временную таблицу, содержащую самую дорогую книгу тематики, например, «Web Technologies».
+CREATE TEMPORARY TABLE maxPriceInTheme
+SELECT NameTheme, NameBook, MAX(Price)
+FROM Books
+         JOIN Themes T ON T.ID_THEME = Books.ID_THEME
+WHERE T.NameTheme = 'Web Development'
+GROUP BY NameTheme;
+
+SELECT *
+FROM maxPriceInTheme;
+
 # 7. Создать запрос, который позволяет вывести всю информацию о
 # работе магазинов. Отсортировать выборку по странам в возрастающем и по названиям магазинов в убывающем порядке.
+
+
 # 8. Создать запрос, показывающий самую популярную книгу
 # 9. Создать временную таблицу, в которой предоставляется информация об авторах, имена которых начинаются с А или В.
+
